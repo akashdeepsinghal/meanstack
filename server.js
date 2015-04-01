685aa4adef27cb3e43bdd0a23c3ff1a19abbb113
akash <akigupta131@gmail.com>
2015-04-02 01:22:06 +0530

    added mongoose

--- server.js
+++ server.js
@@ -14,15 +14,17 @@ var cookieParser 	= require('cookie-parser');
 var session 		= require('express-session');
 var favicon			= require('serve-favicon');
 var errorhandler	= require('errorhandler');
+var mongoose		= require('mongoose');
+
 
 // ROUTES
 // ==============================================
 
-app.use(express.static(__dirname + '/public'));     // set the static files location /public/img will be /img for users
-app.use(morgan('dev'));                     // log every request to the console
-app.use(bodyParser.urlencoded({ extended: false }))    // parse application/x-www-form-urlencoded
-app.use(bodyParser.json())    // parse application/json
-app.use(methodOverride());                  // simulate DELETE and PUT
+app.use(express.static(__dirname + '/app'));		// set the static files location /public/img will be /img for users
+app.use(morgan('dev'));								// log every request to the console
+app.use(bodyParser.urlencoded({ extended: false }));// parse application/x-www-form-urlencoded
+app.use(bodyParser.json());							// parse application/json
+app.use(methodOverride());							// simulate DELETE and PUT
 
 if (process.env.NODE_ENV === 'development') {
   app.use(errorhandler());
@@ -31,6 +33,9 @@ if (process.env.NODE_ENV === 'development') {
 router.get('/', function(req, res) {
     res.send('home');  
 });
+
+mongoose.connect('mongodb://localhost/health');
+mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
 
 // START THE SERVER
 // ==============================================
